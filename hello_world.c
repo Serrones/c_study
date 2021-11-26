b164a18d7f76bcbc33f90cc7cce4c83ce1c699dd
Fábio Serrão <fabio.serrao@luizalabs.com>
2021-11-26 09:12:55 -0300

    feat: add hello_world script

---
+++ hello_world.c
@@ -0,0 +1,18 @@
+/*
+Preprocessor command, which tells a C compiler to include
+stdio.h file before going to actual compilation
+*/
+#include <stdio.h>
+
+/* main function where program execution begins */
+int main()
+{
+    /* 
+    printf is a function available in C which causes the message
+    "Hello, World!" to be displayed on the screen
+    */
+    printf("Hello, World!\n");
+
+    /* Terminates main()function and returns the value 0 */
+    return 0;
+}
